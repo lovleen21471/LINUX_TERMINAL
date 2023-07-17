b3789508279ad7a108a85e3c7974cc32b0ff31a7
Lovleen K Verma <108621655+lovleen21471@users.noreply.github.com>
2023-07-17 23:41:46 +0530

    date.c

--- LINUX_TERMINAL-main/date.c
+++ LINUX_TERMINAL-main/date.c
@@ -3,13 +3,16 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+
 int main(int argc, char* argv[]){
+    // Check the number of command-line arguments
 	if (argc > 2) {
 		fprintf(stderr, "ERROR: Expected max 1 argument, received %d.\n", argc-1);
 		printf("USAGE: %s [-u] [-R]\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 
+    // If no argument is provided, display current time in local format
 	if (argc == 1) {
 		time_t time1;
 		time(&time1);
@@ -19,7 +22,9 @@ int main(int argc, char* argv[]){
 		printf("%s\n", string);
 	}
 	else {
+        // Handle different command-line arguments
 		if (strcmp(argv[1], "-u") == 0) {
+		    // Display current time in UTC format
 			time_t time1;
 			time(&time1);
 			struct tm *information = gmtime(&time1);
@@ -28,9 +33,10 @@ int main(int argc, char* argv[]){
 			printf("%s\n", string);
 		}
 		else if (strcmp(argv[1], "-R") == 0) {
+		    // Display current time in RFC 2822 format
 			time_t time1;
 			time(&time1);
-			struct tm*information = localtime(&time1);
+			struct tm *information = localtime(&time1);
 			char string[100];
 			strftime(string, 100, "%a %b %d %H:%M:%S %z", information);
 			printf("%s\n", string);
