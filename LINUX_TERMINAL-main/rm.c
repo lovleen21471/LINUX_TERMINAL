a1897271db8d20ab6dc22729ea0abec347745696
Lovleen K Verma <108621655+lovleen21471@users.noreply.github.com>
2023-07-17 23:44:05 +0530

    rm.c

--- LINUX_TERMINAL-main/rm.c
+++ LINUX_TERMINAL-main/rm.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>	
+#include <string.h>
+
 int main(int argc, char* argv[]){
-	printf("%d %c %c",argc, argv[0], argv[1]);
+    // Print the number of command-line arguments and the values of argv[0] and argv[1]
+	printf("%d %c %c", argc, argv[0], argv[1]);
+
+    // If the argument is "-i", prompt the user for confirmation before deletion
 	if(!strcmp(argv[1], "-i")){
 		printf("Do you want to delete the file Y/N:");
 		char a;
 		scanf("%c", &a);
+		
+        // Check user input for deletion confirmation
 		if(a == 'Y'){
-			int t = unlink(argv[2]);
+			int t = unlink(argv[2]); // Delete the file
 			
 		}
 		if(a == 'N'){
+			// Do nothing if deletion is not confirmed
 		}
 		else{
-		printf("Not valid input");
+		    printf("Not valid input");
 		}
 	}
+    
+    // If the argument is "-v", delete the file and print the result with verbose output
 	if(!strcmp(argv[1], "-v")){
-		int t = unlink(argv[1]);
+		int t = unlink(argv[1]); // Delete the file
 		if(!t){
 			printf("Enter a valid input\n");
 		}
@@ -27,11 +36,12 @@ int main(int argc, char* argv[]){
 			printf("Removed");
 		}
 	} 
+    
+    // If none of the above conditions match, delete the file and print the result
 	else{
-		int t = unlink(argv[1]);
+		int t = unlink(argv[1]); // Delete the file
 		if(!t){
 			printf("Enter a valid input\n");
-		
 		}
 		else{
 			printf("Removed successfully");
@@ -39,4 +49,3 @@ int main(int argc, char* argv[]){
 	}
 	return 0;
 }
-
