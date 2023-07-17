d12894c4a45bee1cbb92625fef06b91a520cee94
Lovleen K Verma <108621655+lovleen21471@users.noreply.github.com>
2023-07-17 23:39:08 +0530

    Update ls.c

--- LINUX_TERMINAL-main/ls.c
+++ LINUX_TERMINAL-main/ls.c
@@ -5,40 +5,45 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+
 int main(int argc, char* argv[]){
-	struct dirent *dq;
-	DIR *dr = opendir(".");
+    // Variable Declarations
+	struct dirent *dq; // Directory entry structure
+	DIR *dr = opendir("."); // Open current directory
 	if(dr == NULL)
 	{
-		printf("Error");
+		printf("Error"); // Print error if directory cannot be opened
 		return 0;
 	}
-	char **s;
-	int i = 0;
+	char **s; // Array of strings to store file names
+	int i = 0; // Index variable for file name array
+	
+    // Reading directory contents
 	if(argv[1] == "-r"){
 		while((dq = readdir(dr))!= NULL){
-			s[i] = dq->d_name;
+			s[i] = dq->d_name; // Store file name in the array
 			i++;
 		}
 		while(i >=0){
-			printf("%s\t", s[i]);
+			printf("%s\t", s[i]); // Print file names in reverse order
 			i--;
 		}
 	}	
 	if(argv[1] == "-A")
 	{	while((dq = readdir(dr))!= NULL){
 			if(!strcmp(dq->d_name, ".") || !strcmp(dq->d_name, ".."))
-				continue;
-			printf("%s\t", dq->d_name);
+				continue; // Skip current directory and parent directory
+			printf("%s\t", dq->d_name); // Print file name
 		}
 	}
 	else
 	{
 		while((dq = readdir(dr))!= NULL){
-			printf("%s\t", dq->d_name);
+			printf("%s\t", dq->d_name); // Print file name
 		}
 	}
-	closedir(dr);
-	return 0;
+	
+    // Cleanup and return
+	closedir(dr); // Close the directory
+	return 0; // Exit the program
 }
-
